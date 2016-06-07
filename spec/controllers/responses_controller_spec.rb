368e907ada45d7fbf40ca339e377721691ef6b08
timraasveld <ttimitri@gmail.com>
2016-06-07 12:33:16 +0200

    fix controller

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -2,14 +2,14 @@ describe ResponsesController do
   describe '#create' do
     context 'with no answers' do
       it 'includes the first question' do
-        post 'create', response: { questions: [] }
+        post 'create', response: { questions: [], domain_keys: ['POS-PQ'] }
         expect(assigns(:response).next_question.key).to eq 'EL02'
       end
     end
 
-    context 'with answers' do
+    context 'with answers and a domain' do
       it 'includes the next question' do
-        post :create, response: { questions: [{ 'key' => 'EL02', 'answer_value' => 1 }] }, format: 'json'
+        post :create, response: { questions: [{ 'key' => 'EL02', 'answer_value' => 1 }], domain_keys: ['POS-PQ'] }, format: 'json'
         expect(assigns(:response).next_question.key).to eq 'EL03'
       end
     end
