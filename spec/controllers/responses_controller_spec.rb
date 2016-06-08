2dabe3eff165005624b2db5ff6db1ec5cef07677
timraasveld <ttimitri@gmail.com>
2016-06-08 09:04:49 +0200

    WIP

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -1,22 +1,29 @@
 describe ResponsesController do
   describe '#create' do
+    before :each do
+      post :create, params, format: 'json'
+    end
+
     context 'with no answers' do
+      let(:params) { { response: { questions: [], domain_keys: ['POS-PQ'] } } }
+
       it 'includes the first question' do
-        post 'create', response: { questions: [], domain_keys: ['POS-PQ'] }
         expect(assigns(:response).next_question.key).to eq 'EL02'
       end
     end
 
     context 'with answers and a domain' do
+      let(:params) { { response: { questions: [{ 'key' => 'EL02', 'answer_value' => 1 }], domain_keys: ['POS-PQ'] } } }
+
       it 'includes the next question' do
-        post :create, response: { questions: [{ 'key' => 'EL02', 'answer_value' => 1 }], domain_keys: ['POS-PQ'] }, format: 'json'
         expect(assigns(:response).next_question.key).to eq 'EL03'
       end
     end
 
     context 'with wrongly formatted answer' do
+      let(:params) { { response: { questions: [{ 'key' => 'EL02' }] } } }
+
       it 'returns 422' do
-        post :create, response: { questions: [{ 'key' => 'EL02' }] }, format: 'json'
         expect(assigns(:response)).to be_nil
         expect(response.status).to eq 422
       end
