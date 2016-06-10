17182013c0a048118efbd7c84b96112fd56eef88
timraasveld <ttimitri@gmail.com>
2016-06-10 15:12:25 +0200

    Add comment to data_for

--- app/models/r_package.rb
+++ app/models/r_package.rb
@@ -24,6 +24,9 @@ module RPackage
     call('get_itembank_rdata')
   end
 
+  # Retrieve a hash of attributes defined by the R package
+  # for a given set of answers (e.g. 'EL02' => 1)
+  #                 and domain_ids(e.g. ['POS-PQ'])
   def self.data_for(answers, domain_ids)
     raise 'No domains given' unless domain_ids.present?
 
