8ebaf137c2b6237c18a1c7e17cd3155dd78f0d2b
timraasveld <ttimitri@gmail.com>
2016-06-10 16:02:56 +0200

    Add comments to long spec

--- spec/features/choosing_domains_spec.rb
+++ spec/features/choosing_domains_spec.rb
@@ -16,15 +16,20 @@ describe 'choosing domains' do
   end
 
   scenario 'going back and choosing a different domain' do
+    # Start first response
     visit '/'
     click_on 'Positieve symptomen voor psychose'
     expect(page).to have_content 'Vraag 1'
+
+    # Fill out a question
     answer_question 1, 'Eens'
     expect(page).to have_content 'Vraag 2'
+
+    # Start over
     back_button
     click_on 'Positieve symptomen voor psychose'
 
-    # expect previous answer to be removed
+    # expect previous answer to have been removed
     expect(page).to have_content 'Vraag 1'
     expect(page).not_to have_content 'Vraag 2'
   end
