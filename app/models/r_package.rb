d980827fafef1f1aedc6fc5a8bcf0427d4f22a84
timraasveld <ttimitri@gmail.com>
2016-06-22 16:03:01 +0200

    Add intro text

--- app/models/r_package.rb
+++ app/models/r_package.rb
@@ -64,17 +64,10 @@ module RPackage
     Rails.cache.fetch(cache_key_for(function, parameters)) do
       begin
         logged_call function, parameters
-      rescue OpenCPU::Errors::AccessDenied
-        explain_opencpu_configuration
       rescue RuntimeError => e
         raise "Call to R failed with message: #{e.message}"
       end
     end
-  end
-
-  def self.explain_opencpu_configuration
-    raise 'OpenCPU authentication failed. Ensure' \
-      'OPENCPU_ENDPOINT_URL, OPENCPU_USERNAME and OPENCPU_PASSWORD environment variables are set correctly.'
   end
 
   def self.logged_call(function, parameters)
@@ -87,23 +80,6 @@ module RPackage
   end
 
   def self.cache_key_for(function, parameters)
-    "#{ENV['RAILS_ENV']}/#{last_deploy_date}/#{function}/#{parameters}"
-  end
-
-  def self.last_deploy_date
-    description.match(/Packaged: (?<package_date>.*);/).try(:[], :package_date)
-  end
-
-  def self.description
-    Client.instance.description('screensmart')
-  end
-
-  class Client < SimpleDelegator
-    include Singleton
-
-    def initialize
-      @client = OpenCPU.client
-      super(@client)
-    end
+    "#{Rails.env}/R/#{function}/#{parameters}"
   end
 end
