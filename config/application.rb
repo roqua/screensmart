6089d7694a7846a2e0a81aa91de274deef71a8a8
timraasveld <ttimitri@gmail.com>
2016-07-12 16:41:19 +0200

    Limit uuid length

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-# Don't require Rails database stuff
-%w(
-  rails
-  action_controller
-  action_view
-  active_job
-  sprockets
-  active_resource
-).each do |railtie|
-  require railtie
-end
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
