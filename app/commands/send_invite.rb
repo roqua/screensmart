d3c8566838fd87ecd4ad2fedc02324570140b9e0
Arnold van der Meulen <zwippie@gmail.com>
2016-07-13 12:20:19 +0200

    Added SendInvite validation specs

--- app/commands/send_invite.rb
+++ app/commands/send_invite.rb
@@ -4,11 +4,24 @@ class SendInvite < ActiveInteraction::Base
     string
   end
 
+  validates :requester_email, presence: true, email: true
+  validate :validate_domains_defined_by_r_package
+
   def execute
     Events::InviteSent.create!(
       response_uuid: SecureRandom.uuid,
       requester_email: requester_email,
       domains: domains
     )
+  end
+
+  private
+
+  def validate_domains_defined_by_r_package
+    return if domains.empty?
+    domains_not_found = domains - RPackage.domain_ids
+    domains_not_found.each do |domain_not_found|
+      errors.add(:domains, "#{domain_not_found} is not a valid domain")
+    end
   end
 end
