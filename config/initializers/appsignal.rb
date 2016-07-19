438f1f1804feb2c6654329dbca95cc5f025a60b9
timraasveld <ttimitri@gmail.com>
2016-07-19 09:33:22 +0200

    Set envvar correctly

--- config/initializers/appsignal.rb
+++ config/initializers/appsignal.rb
@@ -1,1 +1,1 @@
-ENV['APPSIGNAL_APP_ENV'] = "Heroku #{ENV['HEROKU_APP_NAME'].gsub(/screensmart-?/)}" if ENV['HEROKU_APP_NAME']
+ENV['APPSIGNAL_APP_ENV'] = "Heroku #{ENV['HEROKU_APP_NAME'].gsub(/screensmart-?/, '')}" if ENV['HEROKU_APP_NAME']
