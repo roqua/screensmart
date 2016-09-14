7f040fd3abd03284e54f3e3345dbb1a17b25e205
timraasveld <ttimitri@gmail.com>
2016-09-14 12:57:00 +0300

    Use requester name as display name of invitation email

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,9 +1,13 @@
 class InvitationMailer < ApplicationMailer
-  def invitation_email(requester_email:, respondent_email:, invitation_uuid:)
-    @requester_email = requester_email
+  def invitation_email(requester_name:, respondent_email:, invitation_uuid:)
+    @requester_name = requester_name
     @link = fill_out_url(invitationUUID: invitation_uuid)
 
+    noreply_with_requester_name = Mail::Address.new self.class.default_params[:from]
+    noreply_with_requester_name.display_name = requester_name
+
     mail to: respondent_email,
+         from: noreply_with_requester_name,
          subject: 'Verzoek om vragenlijst in te vullen'
   end
 end
