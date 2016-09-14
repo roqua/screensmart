8b4c5f161c7d82168fb6bcd153e1e9c3cbeeb0f4
timraasveld <ttimitri@gmail.com>
2016-09-14 20:15:33 +0300

    Remove outdated comment

--- app/models/events/invitation_accepted.rb
+++ app/models/events/invitation_accepted.rb
@@ -1,6 +1,5 @@
 module Events
   class InvitationAccepted < Event
-    # No event attributes, invitation_uuid + response_uuid columns are enough
     event_attributes show_secret: :string
 
     def self.find_by_show_secret(show_secret)
