7f40eeb8983f5edfcf4371f46ce87b808795599e
timraasveld <ttimitri@gmail.com>
2017-01-23 09:46:58 +0100

    Undo unrelated change

--- app/models/events/answer_set.rb
+++ app/models/events/answer_set.rb
@@ -13,9 +13,7 @@ module Events
     # Return the last given answer for each question that already has been answered
     def self.last_answers_for(response_uuid)
       question_keys_for(response_uuid).map do |question_id|
-        where(response_uuid: response_uuid).data_contains(question_id: question_id)
-                                           .order(created_at: :desc)
-                                           .limit(1)
+        where(response_uuid: response_uuid).data_contains(question_id: question_id).order(created_at: :desc).limit(1)
       end.flatten
     end
 
