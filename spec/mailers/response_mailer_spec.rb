a131303c6c4562b574950cae09464437eb20712d
Arnold van der Meulen <zwippie@gmail.com>
2017-01-26 11:00:26 +0100

    Generate PDF report for response email (#45)
    * Send pdf with results as attachment
    
    * Use real answers in complete_response method
    
    * Please rubocop
    
    * Removed dead/commented code
    
    * Use dutch filename for PDF attachment
    
    * Add section headers. Use dutch date translation. Use table column width for better presentation.
    
    * Show question intro text in PDF
    
    * Add rake task for faster testing of pdf report
    
    * PDF updated: show full domain name, add whitespace to introduction text
    
    * Show norm_population and quartile in pdf result table

--- spec/mailers/response_mailer_spec.rb
+++ spec/mailers/response_mailer_spec.rb
@@ -2,11 +2,32 @@ describe ResponseMailer do
   describe '#reponse_email' do
     subject { described_class.response_email(params) }
 
+    let(:invitation_sent) { Fabricate :invitation_sent }
+    let(:response) do
+      invitation_accepted = AcceptInvitation.run! invitation_uuid: invitation_sent.invitation_uuid
+      Response.find invitation_accepted.response_uuid
+    end
+
+    def complete_response
+      %w(EL49 EL37 EL03 EL38).each do |question_id|
+        Events::AnswerSet.create!(
+          response_uuid: response.uuid,
+          question_id: question_id,
+          answer_value: 1
+        )
+      end
+    end
+
+    before do
+      allow_any_instance_of(ResponseReport).to receive(:selected_answer_text).and_return('Ja')
+      complete_response
+    end
+
     context 'valid params' do
       let(:params) do
         {
-          requester_email: 'some@doctor.dev',
-          show_secret: SecureRandom.uuid,
+          requester_email: invitation_sent.requester_email,
+          show_secret: response.show_secret,
           invitation_sent_at: Time.zone.now
         }
       end
@@ -16,7 +37,11 @@ describe ResponseMailer do
       end
 
       it 'contains a link to show the filled in questionnaires' do
-        expect(subject.body.encoded).to include("http://test_host/show?showSecret=#{params[:show_secret]}")
+        expect(subject.html_part.body.encoded).to include("http://test_host/show?showSecret=#{params[:show_secret]}")
+      end
+
+      it 'contains a response report pdf as attachment' do
+        expect(subject.attachments.count).to eq(1)
       end
     end
   end
---
+++ spec/pdfs/response_report_spec.rb
@@ -0,0 +1,40 @@
+describe ResponseReport do
+  let(:invitation_sent) { Fabricate :invitation_sent }
+  let(:response) do
+    invitation_accepted = AcceptInvitation.run! invitation_uuid: invitation_sent.invitation_uuid
+    Response.find invitation_accepted.response_uuid
+  end
+
+  def complete_response
+    %w(EL49 EL37 EL03 EL38).each do |question_id|
+      Events::AnswerSet.create!(
+        response_uuid: response.uuid,
+        question_id: question_id,
+        answer_value: 1
+      )
+    end
+  end
+
+  subject { described_class.new(response) }
+
+  before do
+    allow_any_instance_of(ResponseReport).to receive(:selected_answer_text).and_return('Ja')
+    complete_response
+  end
+
+  it 'has at least one page' do
+    expect(subject.page_number).to be >= 1
+  end
+
+  context 'the rendered pdf content' do
+    let(:pdf_content) { PDF::Reader.new(StringIO.new(subject.render)).page(1).to_s }
+
+    it 'contains the introduction text' do
+      expect(pdf_content).to include('In de onderstaande tabel ziet u de resultaten van de ingevulde vragenlijst.')
+    end
+
+    it 'contains the date of the response' do
+      expect(pdf_content).to include('Ingevuld op')
+    end
+  end
+end
