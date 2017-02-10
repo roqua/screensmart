f95a5b5e3cab359295a2c5fae13845351039d406
timraasveld <ttimitri@gmail.com>
2017-02-10 09:03:05 +0100

    Fix SendInvitation params

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,10 +1,14 @@
 class InvitationsController < ApplicationController
   def create
-    outcome = SendInvitation.run(params)
+    outcome = SendInvitation.run(invitation_params.to_h)
     if outcome.valid?
       head :created
     else
       head :bad_request
     end
+  end
+
+  def invitation_params
+    params.permit(:requester_name, :requester_email, :respondent_email, domain_ids: [])
   end
 end
