8570b9ea60cc5c4f6a5a093c3195027040df9054
Tim Raasveld <ttimitri@gmail.com>
2017-04-04 14:40:10 +0200

    Allow overriding hostname in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: ENV.fetch('SCREENSMART_URL', 'localhost:3000') }
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
