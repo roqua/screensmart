627772dc16cf374ebc2236bdff65b27e9d76615e
Tim Raasveld <ttimitri@gmail.com>
2017-04-04 14:51:20 +0200

    Remove useless methods

--- app/models/response_snapshot.rb
+++ app/models/response_snapshot.rb
@@ -3,7 +3,6 @@
 # Uses the results stored in its ResponseFinished event instead of getting info realtime from R.
 class ResponseSnapshot < Response
   delegate :domain_results, :answer_values, to: :response_finished
-  delegate :warning, :estimate, :quartile, :variance, :estimate_interpretation, to: :results
 
   def done
     true
