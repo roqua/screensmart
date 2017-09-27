4d5dd915774b723888177a4c3684c2519d6f3145
Daan Davidsz <daandavidsz@protonmail.com>
2017-09-27 10:49:34 +0200

    Update gems

--- rq-infra/initialize_database.rb
+++ rq-infra/initialize_database.rb
@@ -2,7 +2,7 @@
 
 require 'fileutils'
 
-init_filename = '/cache/database-was-initialized'
+init_filename = '/cache/development-db-was-initialized'
 
 unless File.exist?(init_filename)
   system('bundle exec rake db:create && bundle exec rake db:setup') &&
