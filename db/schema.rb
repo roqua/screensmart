ac88e81bd735655953375f85978e735889b6448e
Daan Davidsz <daandavidsz@protonmail.com>
2017-11-13 12:17:42 +0100

    Add retry

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,7 +15,7 @@ ActiveRecord::Schema.define(version: 20160728121318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "events", force: :cascade do |t|
+  create_table "events", id: :integer, force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.uuid     "response_uuid"
