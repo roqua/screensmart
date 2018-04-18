7b6d11ba7048d32714f6bf03f1c216ac70b2ef28
Arnold van der Meulen <arnold@roqua.nl>
2018-04-18 14:01:44 +0200

    Remove commented line

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # mount Rack::HaproxyStatus::Endpoint.new(path: Rails.root.join('config', 'balancer_state')) => '/load_balancer_status'
   get '/load_balancer_status' => 'roqua/status_checks/status#balancer_status'
 
   root to: 'app#index'
