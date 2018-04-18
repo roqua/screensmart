0b13b8f44a82cd271efcef7d549909cefe6b80cb
Arnold van der Meulen <arnold@roqua.nl>
2018-04-18 13:56:54 +0200

    Add Rails version to migrations

--- db/migrate/20160728121318_add_invitation_uuid_to_events.rb
+++ db/migrate/20160728121318_add_invitation_uuid_to_events.rb
@@ -1,4 +1,4 @@
-class AddInvitationUuidToEvents < ActiveRecord::Migration
+class AddInvitationUuidToEvents < ActiveRecord::Migration[4.2]
   def change
     add_column :events, :invitation_uuid, :uuid
   end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,13 +15,13 @@ ActiveRecord::Schema.define(version: 20160728121318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "events", force: :cascade do |t|
+  create_table "events", id: :serial, force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.uuid     "response_uuid"
-    t.text     "type"
-    t.jsonb    "data"
-    t.uuid     "invitation_uuid"
+    t.uuid "response_uuid"
+    t.text "type"
+    t.jsonb "data"
+    t.uuid "invitation_uuid"
   end
 
 end
